993b76b5d0d38ce2194ca8d0c1461452dc8163c9
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2024-09-19 18:06:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.cpp
+++ Difficulty: Easy/Bubble Sort/bubble-sort.cpp
@@ -25,13 +25,13 @@ class Solution {
         int i,j;
         for(i=0;i<n-1;i++)
         {
-            for(j=0;j<n-1-i;j++)
+            for(j=0;j<n-i-1;j++)
             {
                 if(arr[j]>arr[j+1])
                 {
-                    int temp=arr[j+1];
-                    arr[j+1]=arr[j];
-                    arr[j]=temp;
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
                 }
             }
         }
