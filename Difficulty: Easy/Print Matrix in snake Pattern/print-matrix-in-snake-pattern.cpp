fc7db2d3026031ebdf5418e1ce4053a436f38005
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2025-09-16 14:10:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Matrix in snake Pattern/print-matrix-in-snake-pattern.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> snakePattern(vector<vector<int> > matrix) {
+        // code here
+        vector<int> ans;
+        int row=matrix.size();
+        int col=matrix[0].size();
+        for(int i=0;i<row;i++)
+        {
+            if((i%2)==1)
+            {
+                for(int j=col-1;j>=0;j--)
+                {
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+            else{
+                for(int j=0;j<col;j++)
+                {
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
